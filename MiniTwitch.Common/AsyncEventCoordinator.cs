7440f22053ad3d9f75527092f10d2b077663eb3a
Foretack <observer.in.a.box@gmail.com>
2023-06-26 08:25:50 +0200

    Add .WaitForAll(), XML comments

--- MiniTwitch.Common/AsyncEventCoordinator.cs
+++ MiniTwitch.Common/AsyncEventCoordinator.cs
@@ -1,16 +1,25 @@
 ﻿namespace MiniTwitch.Common;
 
-public class AsyncEventCoordinator<T> : IDisposable
-    where T : notnull
+/// <summary>
+/// Provides mechanisms to asynchronously wait for an event to occur
+/// </summary>
+/// <typeparam name="TEnum">The enum type to operate on</typeparam>
+public class AsyncEventCoordinator<TEnum> : IDisposable
+    where TEnum : struct
 {
     private readonly Dictionary<int, SemaphoreSlim> _locks;
 
+    /// <summary>
+    /// Initializes a new instance of AsyncEventCoordinator
+    /// </summary>
+    /// <param name="initialCount">The initial semaphore request count</param>
+    /// <exception cref="NotSupportedException"></exception>
     public AsyncEventCoordinator(int initialCount = 0)
     {
-        if (!typeof(T).IsEnum)
-            throw new NotSupportedException($"The type {typeof(T)} is not an enum");
+        if (!typeof(TEnum).IsEnum)
+            throw new NotSupportedException($"The type {typeof(TEnum)} is not an enum");
 
-        T[] values = (T[])Enum.GetValues(typeof(T));
+        TEnum[] values = (TEnum[])Enum.GetValues(typeof(TEnum));
         _locks = new(values.Length);
         for (int i = 0; i < values.Length; i++)
         {
@@ -18,15 +27,29 @@ public class AsyncEventCoordinator<T> : IDisposable
         }
     }
 
-    public Task<bool> WaitFor(T value, TimeSpan timeout, CancellationToken cancellationToken = default)
+    /// <summary>
+    /// Asynchronously wait for the <see cref="TEnum"/> value to be released
+    /// </summary>
+    /// <param name="value">The value to wait on</param>
+    /// <param name="timeout">The wait timeout, after which <see langword="false"/> is returned</param>
+    /// <param name="cancellationToken">The cancellation token</param>
+    /// <returns><see langword="true"/> if the wait was successful, otherwise <see langword="false"/></returns>
+    public Task<bool> WaitFor(TEnum value, TimeSpan timeout, CancellationToken cancellationToken = default)
     {
         int key = (int)(object)value;
         return _locks[key].WaitAsync(timeout, cancellationToken);
     }
 
-    public async Task<T?> WaitForAny(IEnumerable<T> values, TimeSpan timeout, CancellationToken cancellationToken = default)
+    /// <summary>
+    /// Asynchronously wait for any of the <see cref="TEnum"/> values to be released
+    /// </summary>
+    /// <param name="values">The values to wait on</param>
+    /// <param name="timeout">The wait timeout, after which <see langword="default"/> is returned</param>
+    /// <param name="cancellationToken">The cancellation token</param>
+    /// <returns>The <see cref="TEnum"/> value that was released, or <see langword="default"/> if the task timed out</returns>
+    public async Task<TEnum> WaitForAny(IEnumerable<TEnum> values, TimeSpan timeout, CancellationToken cancellationToken = default)
     {
-        T[] valuesArr = values.ToArray();
+        TEnum[] valuesArr = values.ToArray();
         Task<bool>[] tasks = valuesArr.Select(x => WaitFor(x, timeout, cancellationToken)).ToArray();
         Task<bool> completed;
         try
@@ -47,19 +70,55 @@ public class AsyncEventCoordinator<T> : IDisposable
         return default;
     }
 
-    public bool IsClosed(T value)
+    /// <summary>
+    /// Asynchronously wait for all the <see cref="TEnum"/> values to be released
+    /// </summary>
+    /// <param name="values">the values to wait on</param>
+    /// <param name="timeout">The wait timeout, after which <see langword="false"/> is returned</param>
+    /// <param name="cancellationToken">The cancellation token</param>
+    /// <returns><see langword="true"/> if the wait was successful, otherwise <see langword="false"/></returns>
+    public async Task<bool> WaitForAll(IEnumerable<TEnum> values, TimeSpan timeout, CancellationToken cancellationToken = default)
     {
-        int key = (int)(object)value;
-        return _locks[key].CurrentCount == 0;
+        TEnum[] valuesArr = values.ToArray();
+        Task[] tasks = valuesArr.Select(x => WaitFor(x, timeout, cancellationToken)).ToArray();
+        try
+        {
+            await Task.WhenAll(tasks).WaitAsync(timeout, cancellationToken);
+        }
+        catch (TimeoutException)
+        {
+            return false;
+        }
+
+        return true;
     }
 
-    public void Open(T value)
+    /// <summary>
+    /// Whether the semaphore for the <see cref="TEnum"/> value can grant a request
+    /// </summary>
+    /// <param name="value">The <see cref="TEnum"/> value to check</param>
+    /// <returns><see langword="true"/> if <see cref="SemaphoreSlim.CurrentCount"/> > 0, otherwise <see langword="false"/></returns>
+    public bool IsReleased(TEnum value)
+    {
+        int key = (int)(object)value;
+        return _locks[key].CurrentCount > 0;
+    }
+
+    /// <summary>
+    /// Release the lock for a <see cref="TEnum"/> value
+    /// </summary>
+    /// <param name="value">The <see cref="TEnum"/> value to release</param>
+    public void Release(TEnum value)
     {
         int key = (int)(object)value;
         _ = _locks[key].Release();
     }
 
-    public void OpenIfClosed(T value)
+    /// <summary>
+    /// Release the lock for a <see cref="TEnum"/> value only if <see cref="SemaphoreSlim.CurrentCount"/> == 0
+    /// </summary>
+    /// <param name="value">The <see cref="TEnum"/> value to release</param>
+    public void ReleaseIfLocked(TEnum value)
     {
         int key = (int)(object)value;
         if (_locks[key].CurrentCount == 0)
