251e517906f23b1c5afb8d2897c3d692abfceec8
Foretack <observer.in.a.box@gmail.com>
2023-07-21 09:55:02 +0200

    Remove unused enums

--- MiniTwitch.PubSub/Enums/LowTrustUserTreatment.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace MiniTwitch.PubSub.Enums;
-
-public enum LowTrustUserTreatment
-{
-    No_Treatment,
-    Active_Monitoring,
-    Restricted
-}
--- MiniTwitch.PubSub/Enums/UserBanEvasionEvaluation.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace MiniTwitch.PubSub.Enums;
-
-public enum UserBanEvasionEvaluation
-{
-    Unknown_Evader,
-    Unlikely_Evader,
-    Likely_Evader,
-    Possible_Evader
-}
