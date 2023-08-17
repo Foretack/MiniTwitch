85f59d0698a60334c2fe4cf12081a5fc0e703b4c
Foretack <observer.in.a.box@gmail.com>
2023-08-17 11:53:43 +0200

    Improve ChannelId & UserId structs with IEquatable, IComparable support

--- MiniTwitch.PubSub/Models/ChannelId.cs
+++ MiniTwitch.PubSub/Models/ChannelId.cs
@@ -4,7 +4,7 @@
 /// A strongly typed object for a channel's ID
 /// <para>Note: This object can be implicitly converted to <see cref="long"/> (returns <see cref="Value"/>)</para>
 /// </summary>
-public readonly struct ChannelId
+public readonly struct ChannelId : IEquatable<ChannelId>, IComparable<ChannelId>
 {
     /// <summary>
     /// The channel ID
@@ -22,4 +22,63 @@ public readonly struct ChannelId
     public static implicit operator ChannelId(long id) => new(id);
     /// <inheritdoc/>
     public static implicit operator long(ChannelId id) => id.Value;
+    /// <inheritdoc/>
+    public int CompareTo(ChannelId other)
+    {
+        if (this.Value < other.Value)
+            return -1;
+        if (this.Value > other.Value)
+            return 1;
+
+        return 0;
+    }
+    /// <inheritdoc/>
+    public bool Equals(ChannelId other) => this.Value == other.Value;
+
+    /// <inheritdoc/>
+    public override bool Equals(object? obj) => obj is ChannelId uid && uid.Value == this.Value;
+
+    /// <inheritdoc/>
+    public override int GetHashCode()
+    {
+        var hash = new HashCode();
+        hash.Add(Value);
+        return hash.ToHashCode();
+    }
+
+    /// <inheritdoc/>
+    public static bool operator ==(ChannelId left, ChannelId right)
+    {
+        return left.Equals(right);
+    }
+
+    /// <inheritdoc/>
+    public static bool operator !=(ChannelId left, ChannelId right)
+    {
+        return !(left == right);
+    }
+
+    /// <inheritdoc/>
+    public static bool operator <(ChannelId left, ChannelId right)
+    {
+        return left.CompareTo(right) < 0;
+    }
+
+    /// <inheritdoc/>
+    public static bool operator <=(ChannelId left, ChannelId right)
+    {
+        return left.CompareTo(right) <= 0;
+    }
+
+    /// <inheritdoc/>
+    public static bool operator >(ChannelId left, ChannelId right)
+    {
+        return left.CompareTo(right) > 0;
+    }
+
+    /// <inheritdoc/>
+    public static bool operator >=(ChannelId left, ChannelId right)
+    {
+        return left.CompareTo(right) >= 0;
+    }
 }
--- MiniTwitch.PubSub/Models/UserId.cs
+++ MiniTwitch.PubSub/Models/UserId.cs
@@ -4,7 +4,7 @@
 /// A strongly typed object for a user's ID
 /// <para>Note: This object can be implicitly converted to <see cref="long"/> (returns <see cref="Value"/>)</para>
 /// </summary>
-public readonly struct UserId
+public readonly struct UserId : IEquatable<UserId>, IComparable<UserId>
 {
     /// <summary>
     /// The user ID
@@ -21,4 +21,63 @@ public readonly struct UserId
     public static implicit operator UserId(long id) => new(id);
     /// <inheritdoc/>
     public static implicit operator long(UserId id) => id.Value;
+    /// <inheritdoc/>
+    public int CompareTo(UserId other)
+    {
+        if (this.Value < other.Value)
+            return -1;
+        if (this.Value > other.Value)
+            return 1;
+
+        return 0;
+    }
+    /// <inheritdoc/>
+    public bool Equals(UserId other) => this.Value == other.Value;
+
+    /// <inheritdoc/>
+    public override bool Equals(object? obj) => obj is UserId uid && uid.Value == this.Value;
+
+    /// <inheritdoc/>
+    public override int GetHashCode()
+    {
+        var hash = new HashCode();
+        hash.Add(Value);
+        return hash.ToHashCode();
+    }
+
+    /// <inheritdoc/>
+    public static bool operator ==(UserId left, UserId right)
+    {
+        return left.Equals(right);
+    }
+
+    /// <inheritdoc/>
+    public static bool operator !=(UserId left, UserId right)
+    {
+        return !(left == right);
+    }
+
+    /// <inheritdoc/>
+    public static bool operator <(UserId left, UserId right)
+    {
+        return left.CompareTo(right) < 0;
+    }
+
+    /// <inheritdoc/>
+    public static bool operator <=(UserId left, UserId right)
+    {
+        return left.CompareTo(right) <= 0;
+    }
+
+    /// <inheritdoc/>
+    public static bool operator >(UserId left, UserId right)
+    {
+        return left.CompareTo(right) > 0;
+    }
+
+    /// <inheritdoc/>
+    public static bool operator >=(UserId left, UserId right)
+    {
+        return left.CompareTo(right) >= 0;
+    }
 }
