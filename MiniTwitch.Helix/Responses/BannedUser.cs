374497ff6b7d6e7dadc6f68a41afe878edd8d581
Foretack <69414142+Foretack@users.noreply.github.com>
2023-11-04 14:03:58 +0100

    Fix string user ID in BannedUser.cs

--- MiniTwitch.Helix/Responses/BannedUser.cs
+++ MiniTwitch.Helix/Responses/BannedUser.cs
@@ -8,7 +8,7 @@ public class BannedUser : BaseResponse<BannedUser.User>
     public record User(
         [property: JsonPropertyName("broadcaster_id")] long BroadcasterId,
         [property: JsonPropertyName("moderator_id")] long ModeratorId,
-        [property: JsonPropertyName("user_id")] string UserId,
+        [property: JsonPropertyName("user_id")] long UserId,
         [property: JsonPropertyName("created_at")] DateTime CreatedAt,
         [property: JsonPropertyName("end_time")] DateTime? EndTime
     );
