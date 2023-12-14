dc9c0ccc7578d28ed2a4d3886e571fbe68b723eb
Foretack <69414142+Foretack@users.noreply.github.com>
2023-12-14 14:43:49 +0100

    Update "Get Ad Schedule" response

---
+++ MiniTwitch.Helix/CHANGELOG.md
@@ -0,0 +1,5 @@
+# MiniTwitch.Helix Changelog
+
+## Upcoming version
+
+- Fix: [Updated "Get Ad Schedule" response](https://dev.twitch.tv/docs/change-log/#:~:text=2023%E2%80%9112%E2%80%9111)
--- MiniTwitch.Helix/Responses/AdSchedule.cs
+++ MiniTwitch.Helix/Responses/AdSchedule.cs
@@ -8,8 +8,8 @@ public class AdSchedule : BaseResponse<AdSchedule.Ad>
     public record Ad(
         [property: JsonPropertyName("next_ad_at")] DateTime? NextAdAt,
         [property: JsonPropertyName("last_ad_at")] DateTime? LastAdAt,
-        [property: JsonPropertyName("length_seconds")] int LengthSeconds,
-        [property: JsonPropertyName("preroll_free_time_seconds")] int PrerollFreeTimeSeconds,
+        [property: JsonPropertyName("duration")] int Duration,
+        [property: JsonPropertyName("preroll_free_time")] int PrerollFreeTime,
         [property: JsonPropertyName("snooze_count")] int SnoozeCount,
         [property: JsonPropertyName("snooze_refresh_at")] DateTime? SnoozeRefreshAt
     );
