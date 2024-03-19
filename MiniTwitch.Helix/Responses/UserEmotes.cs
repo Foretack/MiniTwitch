53889cf7ef0ac742abff41db85201d71e86a6e34
Foretack <69414142+Foretack@users.noreply.github.com>
2024-03-19 12:00:46 +0100

    It will have to be a string for now

--- MiniTwitch.Helix/Responses/UserEmotes.cs
+++ MiniTwitch.Helix/Responses/UserEmotes.cs
@@ -10,7 +10,7 @@ public class UserEmotes : PaginableResponse<UserEmotes.Emote>
         [property: JsonPropertyName("name")] string Name,
         [property: JsonPropertyName("emote_type")] string EmoteType,
         [property: JsonPropertyName("emote_set_id")] string EmoteSetId,
-        [property: JsonPropertyName("owner_id")] long OwnerId,
+        [property: JsonPropertyName("owner_id")] string OwnerId,
         [property: JsonPropertyName("format")] IReadOnlyList<string> Format,
         [property: JsonPropertyName("scale")] IReadOnlyList<string> Scale,
         [property: JsonPropertyName("theme_mode")] IReadOnlyList<string> ThemeMode,
