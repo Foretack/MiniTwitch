49427852753ba350d5c695520d531fe6bbffc9a0
Joachim Flottorp <joachim@flottorp.org>
2024-04-07 19:00:10 +0200

    Typo

--- MiniTwitch.Helix/Responses/UpdatedConduitShards.cs
+++ MiniTwitch.Helix/Responses/UpdatedConduitShards.cs
@@ -8,7 +8,7 @@ public class UpdatedConduitShards : BaseResponse<ConduitShard>
 {
     public IReadOnlyList<Error> Errors { get; init; }
 
-    // TODO: Move 'code' field to an enum if twitch every documents the error codes
+    // TODO: Move 'Code' field to an enum if twitch ever documents the error codes
     public record Error(
         [property: JsonPropertyName("id")]
         string ShardId,
