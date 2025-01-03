a9953ac78912079078e2fb379823549a150f2928
Elias A. <69414142+occluder@users.noreply.github.com>
2025-01-03 13:02:55 +0300

    Add missing ChannelsInformation.Information properties (#141)
    Co-authored-by: Foretack <69414142+Foretack@users.noreply.github.com>

--- MiniTwitch.Helix/Responses/ChannelsInformation.cs
+++ MiniTwitch.Helix/Responses/ChannelsInformation.cs
@@ -1,4 +1,5 @@
 using System.Text.Json.Serialization;
+using MiniTwitch.Helix.Enums;
 using MiniTwitch.Helix.Models;
 
 namespace MiniTwitch.Helix.Responses;
@@ -6,14 +7,16 @@ namespace MiniTwitch.Helix.Responses;
 public class ChannelsInformation : BaseResponse<ChannelsInformation.Information>
 {
     public record Information(
-        long Id,
+        [property: JsonPropertyName("broadcaster_id")] long Id,
         [property: JsonPropertyName("broadcaster_login")] string Name,
         [property: JsonPropertyName("broadcaster_name")] string DisplayName,
-        string Language,
+        [property: JsonPropertyName("broadcaster_language")] string Language,
         string GameName,
         string GameId,
         string Title,
         int Delay,
-        IReadOnlyList<string> Tags
+        IReadOnlyList<string> Tags,
+        IReadOnlyList<ContentLabelId> ContentClassificationLabels,
+        bool IsBrandedContent
     );
 }
