22c10cb29d5de71975c8bf32d2b8fb2d12905644
Foretack <69414142+Foretack@users.noreply.github.com>
2025-01-03 12:59:49 +0300

    Fix id serialization

--- MiniTwitch.Helix/Responses/ChannelsInformation.cs
+++ MiniTwitch.Helix/Responses/ChannelsInformation.cs
@@ -7,7 +7,7 @@ namespace MiniTwitch.Helix.Responses;
 public class ChannelsInformation : BaseResponse<ChannelsInformation.Information>
 {
     public record Information(
-        long Id,
+        [property: JsonPropertyName("broadcaster_id")] long Id,
         [property: JsonPropertyName("broadcaster_login")] string Name,
         [property: JsonPropertyName("broadcaster_name")] string DisplayName,
         [property: JsonPropertyName("broadcaster_language")] string Language,
